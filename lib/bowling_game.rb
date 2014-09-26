a1c230951ede0d3b153d6b4713bc9efb7d464e1a
Dan Bernier and Zach Morek <dev+dbernier+zmorek@continuity.net>
2014-09-26 17:53:14 +0000

    tabs :-(

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,26 +1,26 @@
 class BowlingGame
-	def initialize
-		@score = 0
-		@rolls = []
-	end
+  def initialize
+    @score = 0
+    @rolls = []
+  end
 
-	def roll(pins)
-		if prior_strike?
-			@score += pins
-		end
+  def roll(pins)
+    if prior_strike?
+      @score += pins
+    end
 
-		@rolls << pins
-		@score += pins
-	end
+    @rolls << pins
+    @score += pins
+  end
 
-	def score
-		@score
-	end
+  def score
+    @score
+  end
 
-	private
+  private
 
-	def prior_strike?
-		@rolls[-1] == 10 ||
-			@rolls[-2] == 10
-	end
+  def prior_strike?
+    @rolls[-1] == 10 ||
+      @rolls[-2] == 10
+  end
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -2,33 +2,33 @@ require 'spec_helper'
 require 'bowling_game'
 
 describe BowlingGame do
-	describe 'start-up specs' do
-		it 'is super awesome' do
-			BowlingGame.new
-		end
+  describe 'start-up specs' do
+    it 'is super awesome' do
+      BowlingGame.new
+    end
 
-		it 'can take a roll' do
-			BowlingGame.new.roll(2039)
-		end
+    it 'can take a roll' do
+      BowlingGame.new.roll(2039)
+    end
 
-		it 'has a score' do
-			expect(BowlingGame.new.score).to eq(0)
-		end
-	end
+    it 'has a score' do
+      expect(BowlingGame.new.score).to eq(0)
+    end
+  end
 
-	let(:bowling_game) { BowlingGame.new }
+  let(:bowling_game) { BowlingGame.new }
 
-	it 'keeps track of rolls also roles also hoagies' do
-		bowling_game.roll(1)
-		bowling_game.roll(1)
+  it 'keeps track of rolls also roles also hoagies' do
+    bowling_game.roll(1)
+    bowling_game.roll(1)
 
-		expect(bowling_game.score).to eq(2)
-	end
+    expect(bowling_game.score).to eq(2)
+  end
 
-	it 'can score a strike correctly' do
-		bowling_game.roll(10)
-		bowling_game.roll(1)
-		bowling_game.roll(2)
-		expect(bowling_game.score).to eq(16)
-	end
+  it 'can score a strike correctly' do
+    bowling_game.roll(10)
+    bowling_game.roll(1)
+    bowling_game.roll(2)
+    expect(bowling_game.score).to eq(16)
+  end
 end
