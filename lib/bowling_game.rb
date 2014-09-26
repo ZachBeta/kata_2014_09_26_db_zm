aadb7b1a48d7c9c8fe85f40a60a5b79f6a56c78c
Dan Bernier and Zach Morek <dev+dbernier+zmorek@continuity.net>
2014-09-26 19:31:40 +0000

    ROCK AND ROLL!

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -2,29 +2,94 @@ class BowlingGame
   def initialize
     @score = 0
     @rolls = []
+    @current_frame = Frame.new
   end
 
   def roll(pins)
-    if prior_strike? || prior_spare?
-      @score += pins
-    end
-
-    @rolls << pins
-    @score += pins
+    @current_frame = @current_frame.handle_roll(pins)
   end
 
   def score
-    @score
+    @current_frame.score
+  end
+end
+
+class Frame
+  def initialize(prior_frame = nil)
+    @prior_frame = prior_frame
+    @rolls = []
+    @future_rolls = []
+  end
+
+  def handle_roll(pins)
+    @prior_frame && @prior_frame.handle_future_roll(pins)
+
+    if have_room?
+      stash(pins)
+    else
+      make_new_frame(pins)
+    end
+  end
+
+  def handle_future_roll(pins)
+    if strike? && @future_rolls.size < 2
+      @future_rolls << pins
+    end
+    if spare? && @future_rolls.size < 1
+      @future_rolls << pins
+    end
+
+    @prior_frame && @prior_frame.handle_future_roll(pins)
+  end
+
+  def score
+    # puts self.inspect
+    prior_scores = @prior_frame && @prior_frame.score || 0
+    self_score = @rolls.inject(0, &:+)
+    bonus_score = @future_rolls.inject(0, &:+)
+
+    prior_scores + self_score + bonus_score
+  end
+
+  def inspect
+    [@rolls, @future_rolls].inspect
+  end
+
+  def number
+    if @prior_frame.nil?
+      1
+    else
+      @prior_frame.number + 1
+    end
   end
 
   private
 
-  def prior_strike?
-    @rolls[-1] == 10 ||
-      @rolls[-2] == 10
+  def strike?
+    @rolls.first == 10
   end
 
-  def prior_spare?
-    @rolls.size >= 2 && @rolls[-1] + @rolls[-2] == 10
+  def spare?
+    @rolls.inject(&:+) == 10
+  end
+
+  def have_room?
+    @rolls.size < 2 && @rolls.first.to_i < 10 ||
+      last_frame_shenanigans?
+  end
+
+  def last_frame_shenanigans?
+    (number == 10) && (strike? || spare?) && (@rolls.size < 3)
+  end
+
+  def stash(pins)
+    @rolls << pins
+    self
+  end
+
+  def make_new_frame(pins)
+    Frame.
+      new(self).
+      handle_roll(pins)
   end
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -31,16 +31,67 @@ describe BowlingGame do
     bowling_game.roll(1)
     bowling_game.roll(2)
 
-    expect(bowling_game.score).to eq(10 + 3 + 3)
+    expect(bowling_game.score).to eq(10 + 1 + 2 + 1 + 2)
   end
 
-  it 'can score a spare correctly' do
+  it 'can score a single spare correctly' do
     bowling_game.roll(6)
-    bowling_game.roll(4)
+    bowling_game.roll(4) # Frame A: meh
 
     bowling_game.roll(3)
     bowling_game.roll(1)
 
     expect(bowling_game.score).to eq(10 + 3 + 3 + 1)
+  end
+
+  it 'can score an off-set spare correctly' do
+    bowling_game.roll(4)
+    bowling_game.roll(4)
+
+    bowling_game.roll(6)   # this 'looks like' a spare
+    bowling_game.roll(1)   # this should only count once
+
+    expect(bowling_game.score).to eq(4 + 4 + 6 + 1)
+  end
+
+  it 'gutterballs then turkeys' do
+    9.times do
+      bowling_game.roll(0)
+      bowling_game.roll(0)
+    end
+
+    bowling_game.roll(10)
+    bowling_game.roll(10)
+    bowling_game.roll(10)
+
+    expect(bowling_game.score).to eq 30
+  end
+
+  it 'knows game over'
+
+  it 'scores a perfect game' do
+    9.times do
+      bowling_game.roll(10)
+    end
+
+    bowling_game.roll(10)
+    bowling_game.roll(10)
+    bowling_game.roll(10)
+
+    expect(bowling_game.score).to eq 300
+  end
+
+  it 'handles doule strikes' do
+    bowling_game.roll(10)
+
+    bowling_game.roll(10)
+
+    bowling_game.roll(1)
+    bowling_game.roll(2)
+
+    expected_score = 10 + 10 + 1 +
+      10 + 1 + 2 +
+      1 + 2
+    expect(bowling_game.score).to eq(expected_score)
   end
 end
